6a9d66a004bea695d61737a70770264e011389cf
Barkley <barkley.santo@gmail.com>
2023-03-01 10:30:28 +0800

    add link input field - styel with CTk

--- main.py
+++ main.py
@@ -11,5 +11,20 @@ app = customtkinter.CTk()
 app.geometry("720x480")
 app.title("Youtube Downloader")
 
+
+# Add UI Elements
+title = customtkinter.CTkLabel(
+    app, text="Insert a YouTube link", font=("Arial", 20))
+
+title.pack(pady=10, padx=10)
+
+
+# Link input
+url_var = tkinter.StringVar()
+link = customtkinter.CTkEntry(app, width=350, height=40, textvariable=url_var)
+link.pack(pady=10, padx=10)
+
+
+
 # Run app
 app.mainloop()
